21c0b9f1d96274d3d49a053572f9273270ace62b
Aulus Bezerra <aullus.88@gmail.com>
2023-07-05 18:54:36 -0300

    commit 2

--- App/frontend/src/App.jsx
+++ App/frontend/src/App.jsx
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <div className="h-screen bg-zinc-200">
        <h1 className="text-4xl font-bold">
-      Hello world!
+      Hello world2!
     </h1>
   
     </div>
